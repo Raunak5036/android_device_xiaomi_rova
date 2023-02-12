05d8cf2e8f9b8fa0b411bd8db44df8d9bf3f7f90
Maxx1221 <somanathcharan9@gmail.com>
2023-02-12 19:55:40 +0530

    rova: Droidx-ui bringup

--- droidx_rova.mk
+++ droidx_rova.mk
@@ -10,14 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common bootleggers  stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
 # Inherit from rova device
 $(call inherit-product, device/xiaomi/rova/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rova
-PRODUCT_NAME := blaze_rova
+PRODUCT_NAME := droidx_rova
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4A / 5A
@@ -28,19 +28,21 @@ TARGET_VENDOR := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Some Build Flags
-BLAZE_MAINTAINER := @maxx459
-WITH_GAPPS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USES_BLUR := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
+#DroidXUI props
+IS_OFFICIAL := true
+DROIDX_OFFICIAL := true
+DROIDX_BUILD_TYPE := OFFICIAL
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-blaze
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-droidx
 
 PRODUCT_PACKAGES += \
-    xiaomi_riva_overlay_blaze \
-    xiaomi_rolex_overlay_blaze
+    xiaomi_riva_overlay_droidx \
+    xiaomi_rolex_overlay_droidx
 
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
